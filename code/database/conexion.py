dc8c4f5b7fb12a3773c7175946cb9619339bf16f
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-12 20:30:29 -0600

    Select generico para cualquier tabla

--- code/database/conexion.py
+++ code/database/conexion.py
@@ -29,3 +29,44 @@ def insert(table, **kwargs):
         if conn:
             cursor.close()
             conn.close()
+
+def select(table, *args, condition=None, **kwargs):
+    try:
+        conn = create_connection()
+        cursor = conn.cursor()
+        
+        columns = ",".join(args)
+        if len(columns) == 0:
+            columns = "*"
+
+        conditions = ""
+        if condition:
+            conditions = f" WHERE {condition} "
+        else:
+            for column, value in kwargs.items():
+                if len(conditions) == 0:
+                    conditions += " WHERE "
+                else:
+                    conditions += " AND "
+                conditions += f"{column}='{value}'"
+
+        query = f"SELECT {columns} FROM {str(table)}{conditions};"
+
+        cursor.execute(query)
+
+        query_list = []
+        for row in cursor.fetchall():
+            item = {}
+            for i, column in enumerate(args):
+                item[column] = row[i]
+            query_list.append(item)
+
+        return query_list
+    except mysql.connector.Error as e:
+        print(f"Error at select from table <{table}>\n    query = {query}\n" + str(e))
+        return False
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
