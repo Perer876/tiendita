68894a96e1830aca325460e3e5656c2a339a1b65
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-12 18:09:46 -0600

    Funcion para insertar registros en cualquier tabla

--- code/database/conexion.py
+++ code/database/conexion.py
@@ -9,3 +9,23 @@ def create_connection(user='app_conn', password='superstrongpassword123456', dat
         )
     except mysql.connector.Error as e:
         print ("Error connecting to database\n" + str(e))
+
+def insert(table, **kwargs):    
+    try:
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        columns = ",".join(list(kwargs.keys()))
+        values = ",".join([str(value) if not type(value) == str else f"'{value}'" for value in kwargs.values()])
+        query = f"INSERT INTO {str(table)} ({columns}) VALUES ({values});"
+        
+        cursor.execute(query)
+        conn.commit()
+        return cursor.lastrowid
+    except mysql.connector.Error as e:
+        print(f"Error at insert into table <{table}>\n    query = {query}\n" + str(e))
+        return False
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
