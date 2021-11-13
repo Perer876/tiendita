5d21f8c9e40e22a0d986b5df437e9100696083ea
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-13 16:41:58 -0600

    Declaración métodos del modulo <listaproductos>

---
+++ code/database/listaproductos.py
@@ -0,0 +1,16 @@
+from database.conexion import insert, update, select, delete
+
+def agregar(id_venta:int, lista_productos:list):
+    pass
+
+def editar(id_venta:int, lista_productos:list):
+    pass
+
+def mostrar(id_venta:int):
+    pass
+
+def eliminar(id_venta:int):
+    pass
+
+def calcular_valor(id_venta:int):
+    pass
