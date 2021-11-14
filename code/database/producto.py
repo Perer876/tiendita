8c650518277ef8456476f7600f2321fa8337fc2f
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-13 20:14:14 -0600

    Extendida funcionalidad del metodo <mostrar>

--- code/database/producto.py
+++ code/database/producto.py
@@ -12,8 +12,10 @@ def editar(datos:dict, condicion:str=None, **condiciones):
 def eliminar(condicion:str=None, **condiciones):
     return delete("producto", condition=condicion, **condiciones)
 
-def mostrar(id_producto:int):
-    return select("producto", "nombre", "precio", "codigo", id=id_producto)
+def mostrar(id_producto:int, *columnas):
+    if len(columnas) == 0:
+        columnas = ["nombre", "precio", "codigo"]
+    return select("producto", *columnas, id=id_producto)[0]
 
 def cantidad(condicion:str=None, **condiciones):
     return select("producto", "count(*)", condition=condicion, **condiciones)[0]["count(*)"]
