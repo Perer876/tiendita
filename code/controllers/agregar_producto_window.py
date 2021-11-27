52f9fab91e69d4ac660008498077a1b59e783716
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-27 00:55:21 -0600

    Agregar productos funcionando

--- code/controllers/agregar_producto_window.py
+++ code/controllers/agregar_producto_window.py
@@ -1,5 +1,5 @@
 from PySide2.QtWidgets import QMainWindow
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, Slot
 from views.ui_agregar_producto_window import Ui_agregar_producto_window
 
 class AgregarProductoWindow(QMainWindow):
@@ -8,3 +8,33 @@ class AgregarProductoWindow(QMainWindow):
         self.ui = Ui_agregar_producto_window()
         self.ui.setupUi(self)
         self.setWindowFlag(Qt.Window)
+        self.ui.agregar_pushButton.clicked.connect(self.agregar)
+        self.ui.cancelar_pushButton.clicked.connect(self.cancelar)
+
+    @Slot()
+    def agregar(self):
+        self.ui.nombre_warning.clear()
+        self.ui.precio_warning.clear()
+        self.ui.codigo_warning.clear()
+
+        from database.producto import Producto, agregar
+        p = Producto()
+        p.nombre = self.ui.nombre_lineEdit.text()
+        p.precio = self.ui.precio_doubleSpinBox.value()
+        p.codigo = self.ui.codigo_lineEdit.text()
+
+        if len(p.nombre) == 0:
+            self.ui.nombre_warning.setText("El nombre no puede estar en blanco")
+        elif p.precio <= 0:
+            self.ui.precio_warning.setText("el precio no puede ser cero")
+        elif len(p.codigo) == 0:
+            self.ui.codigo_warning.setText("El codigo no puede estar en blanco")
+        else:
+            print(agregar(p.to_dict()))
+            self.ui.nombre_lineEdit.clear()      
+            self.ui.precio_doubleSpinBox.setValue(0.00)
+            self.ui.codigo_lineEdit.clear()
+        
+    @Slot()
+    def cancelar(self):
+        self.close()
