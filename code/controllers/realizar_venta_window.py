d6150a41d912c96f77d60ddf2764c1fbe44c923c
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-11-28 16:49:35 -0600

    Aumentar y disminuir cantidad de productos en venta

--- code/controllers/realizar_venta_window.py
+++ code/controllers/realizar_venta_window.py
@@ -12,8 +12,13 @@ class RealizarVentaWindow(QMainWindow):
 
         self.ui.cancelar_pushButton.clicked.connect(self.cancelar_venta)
         self.ui.agregar_producto_pushButton.clicked.connect(self.agregar_producto)
+        self.ui.aumentar_pushButton.clicked.connect(self.aumentar_producto)
+        self.ui.disminuir_pushButton.clicked.connect(self.disminuir_producto)
+
+        self.ui.termino_producto_lineEdit.returnPressed.connect(self.agregar_producto)
 
         self.venta = venta.Venta()
+        self.last_row = -1
 
         self.iniciar_tabla_venta()
         self.iniciar_productos_comboBox()
@@ -44,20 +49,48 @@ class RealizarVentaWindow(QMainWindow):
 
         self.actualizar_vista_venta()
 
+    @Slot()
+    def aumentar_producto(self):
+        row = self.ui.venta_tableWidget.currentRow()
+        if row >= 0:
+            self.last_row = row
+
+        if self.last_row < 0:
+            QMessageBox.warning(self, "Atención", "Seleccione algún producto")
+        else:
+            id_producto = self.ui.venta_tableWidget.item(self.last_row, 0).text()
+            self.venta.agregar_producto(id_producto, 1)
+            self.actualizar_vista_venta()
+
+    @Slot()
+    def disminuir_producto(self):
+        row = self.ui.venta_tableWidget.currentRow()
+        if row >= 0:
+            self.last_row = row
+
+        if self.last_row < 0:
+            QMessageBox.warning(self, "Atención", "Seleccione algún producto")
+        else:
+            id_producto = self.ui.venta_tableWidget.item(self.last_row, 0).text()
+            self.venta.agregar_producto(id_producto, -1)
+            self.actualizar_vista_venta()
+
     def iniciar_tabla_venta(self):
-        self.ui.venta_tableWidget.setColumnCount(4)
-        self.ui.venta_tableWidget.setColumnWidth(0, 502)
-        self.ui.venta_tableWidget.setColumnWidth(1, 169)
-        self.ui.venta_tableWidget.setColumnWidth(1, 169)
-        self.ui.venta_tableWidget.setColumnWidth(1, 169)
-        self.ui.venta_tableWidget.setHorizontalHeaderLabels(["Producto", "Precio Unitario", "Cantidad", "Subtotal"])
+        self.ui.venta_tableWidget.setColumnCount(5)
+        self.ui.venta_tableWidget.setColumnWidth(1, 502)
+        self.ui.venta_tableWidget.setColumnWidth(2, 169)
+        self.ui.venta_tableWidget.setColumnWidth(3, 169)
+        self.ui.venta_tableWidget.setColumnWidth(4, 169)
+        self.ui.venta_tableWidget.setHorizontalHeaderLabels(["id", "Producto", "Precio Unitario", "Cantidad", "Subtotal"])
+        self.ui.venta_tableWidget.hideColumn(0)
 
     def insertar_subtotal_en_tabla(self, lp:listaproductos.ProductoVenta, pos):
         self.ui.venta_tableWidget.insertRow(pos)
-        self.ui.venta_tableWidget.setItem(pos, 0, QTableWidgetItem(str(lp.nombre_producto)))
-        self.ui.venta_tableWidget.setItem(pos, 1, QTableWidgetItem(str(lp.precio_producto)))
-        self.ui.venta_tableWidget.setItem(pos, 2, QTableWidgetItem(str(lp.cantidad)))
-        self.ui.venta_tableWidget.setItem(pos, 3, QTableWidgetItem(str(lp.sub_total)))
+        self.ui.venta_tableWidget.setItem(pos, 0, QTableWidgetItem(str(lp.id_producto)))
+        self.ui.venta_tableWidget.setItem(pos, 1, QTableWidgetItem(str(lp.nombre_producto)))
+        self.ui.venta_tableWidget.setItem(pos, 2, QTableWidgetItem(str(lp.precio_producto)))
+        self.ui.venta_tableWidget.setItem(pos, 3, QTableWidgetItem(str(lp.cantidad)))
+        self.ui.venta_tableWidget.setItem(pos, 4, QTableWidgetItem(str(lp.sub_total)))
 
     def vaciar_tabla_venta(self):
         self.ui.venta_tableWidget.setRowCount(0)
@@ -66,7 +99,8 @@ class RealizarVentaWindow(QMainWindow):
         self.vaciar_tabla_venta()
         for i, lp in enumerate(self.venta.lista_productos):
             self.insertar_subtotal_en_tabla(lp, i)
-        self.ui.total_label.setText(str(self.venta.total))
+        total = f'<html><head/><body><p><span style=" font-size:24pt;">${str(self.venta.total)}</span></p></body></html>'
+        self.ui.total_label.setText(total)
 
     def iniciar_productos_comboBox(self):
         self.ui.buscar_producto_por_comboBox.addItem("Nombre")
